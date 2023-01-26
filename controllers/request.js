7389e776df4ca7074906a15c0fa671b1560b3bf9
kasperis3 <kushaal@berkeley.edu>
2023-01-26 15:04:16 -0800

    endpoint post tst

--- controllers/request.js
+++ controllers/request.js
@@ -6,7 +6,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const SQL = new PG(); 
 
 requestRouter.post('/', async (req, res, next) => {
-  console.log(req.url, req.originalUrl, req.baseUrl);
+  console.log(req.hostname);
   const randomUrl = req.hostname.split(".")[0]; // SWITCH BACK if route path adjusts
   // const randomUrl = req.params.url;
   const rawRequest = new RequestModel({
